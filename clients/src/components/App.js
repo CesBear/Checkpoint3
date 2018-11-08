ab911a56a48ed769f5b59b70c8188ce6b430d44e
Cesar Ramirez <cramirez@intersysconsulting.com>
2018-11-07 20:15:05 -0600

    adding client data

---
+++ clients/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../assets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+            Test App
+      </div>
+    );
+  }
+}
+
+export default App;
